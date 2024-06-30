990c1749df00d653581619086e1df9b4461322a9
hmiche <mehdi.hmiche@gmail.com>
2024-06-30 01:29:28 +0100

    my first commit

---
+++ src/test/java/com/mido/erp/ErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mido.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
